86aa9a67198a81e80159046e6a0e5e4301ed92d9
alphen11330 <alphen11330@gmail.com>
2025-04-13 16:25:26 +0900

    20250413

---
+++ .vscode/.history/pixelator/src/app/ditherjs.d_20250413033502.ts
@@ -0,0 +1,12 @@
+declare module 'ditherjs' {
+    interface DitherOptions {
+        palette: number[][];
+    }
+
+    class Dither {
+        constructor(options: DitherOptions);
+        dither(imageData: any): any;
+    }
+
+    export default Dither;
+}
